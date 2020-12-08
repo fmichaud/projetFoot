79204cca1d499363d7978cf587d69dc92a614271
François Michaud <fmichaud@9fevrier.com>
2020-12-08 21:03:04 +0100

    docker-compose

---
+++ project/controllers/upload.controllers.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+const bunyan = require('bunyan')
+const { Article } = require('../models')
+
+/**
+ * The logger.
+ * *
+ * @type {Logger}
+ */
+const LOG = bunyan.createLogger({name: __filename})
+
+
+
+const uploadFiles = async (req, res) => {
+  try {
+    LOG.debug('File', {file: req.file})
+
+    if (req.file == undefined) {
+      return res.send(`You must select a file.`)
+    }
+    Image.create({
+      type: req.file.mimetype,
+      name: req.file.originalname,
+      data: fs.readFileSync(
+        __basedir + '/resources/static/assets/uploads/' + req.file.filename
+      ),
+    }).then((image) => {
+      fs.writeFileSync(
+        __basedir + '/resources/static/assets/tmp/' + image.name,
+        image.data
+      )
+
+      return res.send(`File has been uploaded.`)
+    })
+  } catch (error) {
+    LOG.error('An error occurs', {error})
+    return res.send(`Error when trying upload images: ${error}`)
+  }
+}
+
+module.exports = {
+  uploadFiles,
+}
